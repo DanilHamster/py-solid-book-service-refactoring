f0b343cade378b94824c0641278e08d588c15988
Hamster <dtaranenko137@gmail.com>
2025-09-28 12:34:33 +0300

    done

---
+++ app/printer.py
@@ -0,0 +1,18 @@
+from app.book import Book
+from abc import ABC, abstractmethod
+
+
+class PrintStrategy(ABC):
+    @abstractmethod
+    def print(self, book: Book) -> None:
+        pass
+
+class ConsolePrinter(PrintStrategy):
+    def print(self, book: Book) -> None:
+        print(f"Printing the book: {book.title}...")
+        print(book.content)
+
+class ReversePrinter(PrintStrategy):
+    def print(self, book: Book) -> None:
+        print(f"Printing the book in reverse: {book.title}...")
+        print(book.content[::-1])
